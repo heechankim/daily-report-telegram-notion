a535b75900d51d96d3955708980c3009f259c54c
heechankim <heechan.dev@gmail.com>
2022-01-24 21:28:20 +0900

    Feat: Bot send a message at ten minutes to every o'clock

--- daily_report.py
+++ daily_report.py
@@ -16,10 +16,17 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 # env
 from dotenv import dotenv_values
 
-from pytz import timezone
-import time
 import datetime
 
+# calculate run_repeating's first parameter
+def get_time_delay_to_start():
+    now = datetime.datetime.now()
+    start = now.replace(minute=50, second=00)
+
+    if now.minute in range(50, 60):
+        start += datetime.timedelta(hours=1)
+
+    return start - now
 
 class App:
     def __init__(self):
@@ -28,26 +35,26 @@ class App:
         self.updater = Updater(token=self.ENV['TELEGRAM_BOT_TOKEN'], use_context=True)
         self.dispatcher = self.updater.dispatcher
         self.job_queue = self.updater.job_queue
+        self.count = 0
 
     def rp_command(self, update: Update, context: CallbackContext):
         context.bot.send_message(chat_id=self.ENV['TELEGRAM_CHAN_ID'], text="command rp testing")
 
-    def callback_minute(self, context: CallbackContext):
+    def callback_report_alarm(self, context: CallbackContext):
+        self.count += 1
         context.bot.send_message(chat_id=self.ENV['TELEGRAM_CHAN_ID'],
-                                 text="One message every minute\n" + time.strftime('%c', time.localtime(time.time())))
+                                 text="report alarm! [" + str(self.count) + "]\n" + datetime.datetime.now().isoformat(timespec='seconds'))
 
     def start(self):
 
         self.rp_handler = CommandHandler('rp', self.rp_command)
         self.dispatcher.add_handler(self.rp_handler)
 
-        job_minute = self.job_queue.run_repeating(self.callback_minute, interval=60, first=10)
+        job_daily_report = self.job_queue.run_repeating(
+            self.callback_report_alarm,
+            interval=datetime.timedelta(hours=1),
+            first=get_time_delay_to_start()
+        )
 
         self.updater.start_polling()
         self.updater.idle()
-
-
-
-    def __del__(self):
-        self.updater.stop()
-        print("########## shutdown app ##########")
